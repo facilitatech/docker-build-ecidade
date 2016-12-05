b020c5d6d9b07d695d16db662a3c3c2b01b7628d
Lucas Alves <luk4z_7@hotmail.com>
2016-12-05 10:42:28 -0300

    Criado arquivo para configuração e parametrização
    	* Configurações do e-cidade para download das versões
    	  corretas, links e outros parâmetros

---
+++ config.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# link do pacote disponibilizado no portal do software público
+sourcecode=0
+
+# preencher somente se possuir a base de dados separado dos fontes
+database=0
+
+# Versão do e-cidade
+versao='2.3.55'
